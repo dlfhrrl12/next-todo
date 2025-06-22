378b3a2e725be71fc6ef2532855fff813bace167
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-06-23 05:22:49 +0900

    feat: 메인 페이지 UI 레이아웃 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="mx-auto max-w-2xl py-12 px-4">
+      <h1 className="text-center text-4xl font-bold">Next-TodoList</h1>
+      <div className="text-center my-8">
+        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 transition-colors">
+          New
+        </button>
+      </div>
+      <ul>
+        <li>1 번째 일</li>
+        <li>2 번째 일</li>
+        <li>3 번째 일</li>
+      </ul>
+    </div>
+  );
 }
